6c58bda049b27179c36ef9b22807fb5e51245a38
Vitor Moura <663103+vitormoura@users.noreply.github.com>
2021-11-15 20:54:44 -0500

    rajoute le pattern PRG au form signup

--- musiquepy_website/app/routes/accounts.py
+++ musiquepy_website/app/routes/accounts.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
+from flask.helpers import make_response, url_for
+from werkzeug.datastructures import ImmutableMultiDict
+from werkzeug.utils import redirect
 from ..models.form_signup import FormSignup
 
 bp = Blueprint('accounts', __name__, url_prefix='/accounts')
 
-@bp.route("/signup", methods=('post', 'get'))
-def page_signup():
-    form = FormSignup(request.form)
+@bp.route("/signup", methods=['get'])
+def get_page_signup():
+    form = FormSignup()
+    submitted = False
 
-    if request.method == 'POST':
+    if 'form_data' in session:
+        
+        submitted = True
+        form_data = session.pop('form_data')
+        form.process(ImmutableMultiDict(form_data))
         form.validate()
-
-        # TODO: Enregistrer nouvel utilisateur
     
-    return render_template('signup.html', form=form, submitted=request.method == 'POST')
+    return render_template('signup.html', form=form, submitted=submitted)
+
+@bp.route("/signup", methods=['post'])
+def post_page_signup():
+
+    form = FormSignup(request.form)
+    
+    if not form.validate():
+        session['form_data'] = request.form.to_dict()
+        return redirect(url_for('accounts.get_page_signup'))
+
+    # TODO: Enregistrer nouvel utilisateur
+    return make_response('signup ok')
