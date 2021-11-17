2dd1890f4de993e80d07c8cf0e76ea8b8b62214f
Vitor Moura <663103+vitormoura@users.noreply.github.com>
2021-11-16 21:23:21 -0500

    corrige des erreurs html

--- musiquepy_website/app/models/form_signup.py
+++ musiquepy_website/app/models/form_signup.py
@@ -1,9 +1,14 @@
 from wtforms import Form, validators
 from wtforms.fields.simple import BooleanField, StringField
 
+
 class FormSignup(Form):
-    user_email = StringField("Quel est votre courriel?", validators=[validators.InputRequired(), validators.Email()])
-    user_email_confirm = StringField("Confirmez votre courriel", validators=[validators.InputRequired(), validators.Email(), validators.EqualTo('user_email')])
-    user_password=StringField("Créez un mot de passe", validators=[validators.InputRequired(), validators.Regexp('^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{8,20}$')])
-    user_nickname=StringField("Nous vous appelerons comment?", validators=[validators.InputRequired()])
-    user_marketing_ok=BooleanField("Partagez des données pour le marketing")
+    user_email = StringField("Quel est votre courriel?", validators=[
+                             validators.InputRequired(), validators.Email()])
+    user_email_confirm = StringField("Confirmez votre courriel", validators=[
+                                     validators.InputRequired(), validators.Email(), validators.EqualTo('user_email')])
+    user_password = StringField("Créez un mot de passe", validators=[validators.InputRequired(
+    ), validators.Regexp('^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])[a-zA-Z0-9]{8,20}$')])
+    user_nickname = StringField("Nous vous appelerons comment?", validators=[
+                                validators.InputRequired()])
+    user_marketing_ok = BooleanField("Partagez des données pour le marketing")
--- musiquepy_website/app/routes/accounts.py
+++ musiquepy_website/app/routes/accounts.py
@@ -6,25 +6,27 @@ from ..models.form_signup import FormSignup
 
 bp = Blueprint('accounts', __name__, url_prefix='/accounts')
 
+
 @bp.route("/signup", methods=['get'])
 def get_page_signup():
     form = FormSignup()
     submitted = False
 
     if 'form_data' in session:
-        
+
         submitted = True
         form_data = session.pop('form_data')
         form.process(ImmutableMultiDict(form_data))
         form.validate()
-    
+
     return render_template('signup.html', form=form, submitted=submitted)
+
 
 @bp.route("/signup", methods=['post'])
 def post_page_signup():
 
     form = FormSignup(request.form)
-    
+
     if not form.validate():
         session['form_data'] = request.form.to_dict()
         return redirect(url_for('accounts.get_page_signup'))
